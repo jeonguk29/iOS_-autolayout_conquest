35d3d9d71d951336d9b994cd6cc4ddc8ecd403dd
woogie <jeonguk29@naver.com>
2023-05-02 00:47:35 +0900

    오토레이아웃 정복 : day1 : 기본 핵심 총 4가지(x:y 위치, 가로:세로크기)알려줘야 오토레이아웃을 잡을 수 있음
    개념 - 레이아웃을 자동으로 계산
    
    핵심
    	해당 뷰의 x,y 위치 : 엥커로 제공
    	해당 뷰의 가로, 세로 크기( 직접 사이즈를 주던가)
    	- 엥커기반으로 4가지 엥커를 다 주게 된다면 사이즈 또한 자동으로 알 수 있음

---
+++ day1_Autolayout_basic_concept/day1_Autolayout_basic_concept/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day1_Autolayout_basic_concept
+//
+//  Created by 정정욱 on 2023/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
